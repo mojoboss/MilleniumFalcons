0f7f977e558f5cbb81c25da8f8a91c04d4749f5c
animusmojo <animusmojo@gmail.com>
2018-11-01 16:49:00 +0530

    handling inputs at the EE level

--- MFS/Assets/Scripts/GameExperienceEngine.cs
+++ MFS/Assets/Scripts/GameExperienceEngine.cs
@@ -1,14 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Zenject;
+
+public static class C
+{
+	public enum KeyType
+	{
+		LEFT_KEY,
+		RIGHT_KEY
+	}
+
+	public static Dictionary<KeyCode, KeyType> KeyMapping = new Dictionary<KeyCode, KeyType>()
+	{
+		{KeyCode.LeftArrow, KeyType.LEFT_KEY}
+		,{KeyCode.RightArrow, KeyType.RIGHT_KEY}
+	};
+}
+
 
 public class GameExperienceEngine : MonoBehaviour
 {
 
 	[Inject] private GameSceneViewModel _ViewModel;
-	// Use this for initialization
-	void Start () {
+
+	public void HandleMovement(KeyCode key, Action<C.KeyType> onComplete)
+	{
+		//TODO: Do something later
+		onComplete(C.KeyMapping[key]);
 
 	}
 
