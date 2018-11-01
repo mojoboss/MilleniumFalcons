477ee64c07456458a00e35d8e08ef87959eaae64
animusmojo <animusmojo@gmail.com>
2018-11-01 20:17:45 +0530

    handling movement in service WIP

--- MFS/Assets/Scripts/OfflineGameService.cs
+++ MFS/Assets/Scripts/OfflineGameService.cs
@@ -9,5 +9,19 @@ public class OfflineGameService
 	[Inject] private GameController _Controller;
 	[Inject] private GameState _State;
 
+	public void SetupGame()
+	{
+		_State.SetMyPlayerId(0);
+	}
 
+	public void MoveOnKeyPress(C.KeyType type)
+	{
+		SetupGame();
+		int _myPlayerId = _State.GetMyPlayerId();
+		if (type == C.KeyType.LEFT_KEY)
+		{
+			_State.SetPlayerVelocityX(_myPlayerId, -1);
+			float velX = _State.GetPlayerVel(_myPlayerId);
+		}
+	}
 }
